abaee8d0bdf8e1fa1ad2745082ac70f65f50a500
Florin Popa <456185+FlorinPopaCodes@users.noreply.github.com>
2024-12-06 17:56:42 +0200

    Add day 5

---
+++ 2024/Day5/day5_2.rb
@@ -0,0 +1,53 @@
+require 'debug'
+
+input_lines = File.readlines('./2024/Day5/input')
+
+pages_input = false
+order = {} # [a,b] => true, false b > a => true, default 0
+pages = [] # Array of Array of ints
+
+def sorted(a, o)
+  a.sort { |p1, p2|  o[[p1 , p2]] || - o[[p2, p1]] } # Bug generator because I didn't consider ordering 
+end
+
+input_lines.each_with_index do |row, i|
+  if row == "\n"
+    pages_input = true 
+    next
+  end
+
+  if pages_input
+    pages << row.split(',').map(&:to_i)
+  else
+    a, b = row.split('|').map(&:to_i)
+    order[[a,b].sort] = b > a ? -1 : 1 # Bug generator in ordering
+  end
+end
+
+p "orders #{order.size}"
+p "pages #{pages.size}"
+
+# 47|26
+#  order.select { |k, _| k[0] == 68 || k[1] == 47 }.select { |k, _| k[0] == 26 || k[1] == 26 }
+# debugger
+
+grouped_pages = pages.group_by do |page|
+    p page
+    p sorted(page, order)
+    page == sorted(page, order) ? :correct : :incorrect
+  end
+  
+# p "correct"
+# p grouped_pages[:correct]
+# p "incorrect"
+# p grouped_pages[:incorrect]
+
+
+p grouped_pages
+p grouped_pages[:correct].sum { |page| page[page.size/2] }
+p grouped_pages[:incorrect].sum { |page| sorted(page, order)[page.size/2] }
+
+# # result was too low.
+# p mids.sum
+
+
