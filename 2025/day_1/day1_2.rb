b165340dce204e79df960b818d8c100320f92204
Florin Popa <456185+FlorinPopaCodes@users.noreply.github.com>
2025-12-01 15:50:02 +0200

    Add 2025.day1

---
+++ 2025/day_1/day1_2.rb
@@ -0,0 +1,28 @@
+input_lines = File.readlines('./2025/day_1/input')
+
+
+password = 0
+current = 50
+
+input_lines.each do |line|
+  l = line.chomp
+
+  direction = l[0]
+  steps = l[1..].to_i
+  
+  if direction == "L"
+    current -= steps
+  else
+    current += steps
+  end
+
+  if current < 0 || current > 99
+    d, m = current.divmod(100)
+
+    password += d.abs
+    current = m
+  end
+
+end
+
+p password
